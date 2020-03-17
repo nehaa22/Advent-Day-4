f5d0dc0861a2a1646d312fe8e7b848aa794008ec
snehamanojraopalekar <nehapalekar026@gmail.com>
2020-03-17 19:00:37 +0530

    Add. password should have repeated number once

--- Password.py
+++ Password.py
@@ -6,5 +6,13 @@ class Password(object):
         for i in range(len(value) - 1):
             if value[i] > value[i + 1]:
                 return False
-        return True
+
+        for i in range(len(value) - 1):
+            for i in range(len(value) - 1):
+                if value[i] == value[i + 1]:
+                    if (i > 0 and value[i - 1] == value[i]) or (
+                            i < len(value) - 2 and value[i + 2] == value[i + 1]):
+                        continue
+                    else:
+                        return True
 
--- test_Password.py
+++ test_Password.py
@@ -6,9 +6,15 @@ from Password import Password
 class PasswordTest(unittest.TestCase):
 
     def test_Pass_if_number_should_be_in_increasing_order(self):
-        number = 12345
+        number = 123445
         password = Password()
         self.assertTrue(password.validate(number))
+
+    def test_Fail_if_number_should_be_in_increasing_order(self):
+        number = 122456
+        password = Password()
+        self.assertTrue(password.validate(number))
+
 
 
 
